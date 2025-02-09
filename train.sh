c50df8adbdefa1ca863142633a64f1713c2e970f
Kyle Gallatin <kylegallatin@gmail.com>
2025-02-09 19:48:39 +0000

    update machine type

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 gcloud ai custom-jobs create \
   --region=us-central1 \
   --display-name=train1 \
-  --worker-pool-spec=machine-type=n2-standard-1,replica-count=1,executor-image-uri=us-docker.pkg.dev/vertex-ai/training/sklearn-cpu.1-0:latest,local-package-path=.,script=task.py
+  --worker-pool-spec=machine-type=ct5lp-hightpu-1t,replica-count=1,executor-image-uri=us-docker.pkg.dev/vertex-ai/training/sklearn-cpu.1-0:latest,local-package-path=.,script=task.py
